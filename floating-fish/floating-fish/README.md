0181243cc3f8c37991073b6fe4228159fa489360
Ahmed WAeer <ahmedwaeer@gmail.com>
2021-04-11 19:58:04 +0200

    floating fish

---
+++ floating-fish/floating-fish/README.md
@@ -0,0 +1,6 @@
+### The Floating Fish PWA (Progressive Web App)
+
+To generate new css file based on tailwind css, run this command:
+```
+npx tailwindcss-cli@latest build -o css/tailwind.css
+```
---
+++ floating-fish/floating-fish/js/script.js
@@ -0,0 +1,16 @@
+const styles = [
+    {
+        img: 'https://www.pinterest.com/pin/5136987063769128/',
+        styles: ['bohemian']
+    },
+    {
+        img: 'https://www.pinterest.com/pin/454089574922886416/',
+        styles: ['bohemian', 'modern']
+    },
+    {
+        img: 'https://www.pinterest.com/pin/5136987063769128/',
+        styles: ['classic']
+    },
+]
+
+
